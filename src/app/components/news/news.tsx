b52feca39437ef640eb23372371aba8cc38a7253
André <vestrheim-kvist@hotmail.com>
2024-11-22 10:08:50 +0100

    making patch cards

---
+++ src/app/components/news/news.tsx
@@ -0,0 +1,16 @@
+import "./news.css";
+
+export default function news() {
+  return (
+    <section className="news">
+      <aside>
+        <h3>Patch</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur </p>
+      </aside>
+      <aside>
+        <h3>Patch</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur </p>
+      </aside>
+    </section>
+  );
+}
