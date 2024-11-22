7d79ed194b7f150ab83ed04968b94eeddf1d3577
André <vestrheim-kvist@hotmail.com>
2024-11-22 10:54:52 +0100

    shorter code

--- src/app/components/news/news.tsx
+++ src/app/components/news/news.tsx
@@ -2,15 +2,17 @@ import "./news.css";
 
 export default function news() {
   return (
-    <section className="news">
-      <aside>
+    <section className="flex-row news">
+      <button className="btn-news">Back</button>
+      <aside className="flex-column">
         <h3>Patch</h3>
         <p>Lorem ipsum dolor sit amet, consectetur </p>
       </aside>
-      <aside>
+      <aside className="flex-column">
         <h3>Patch</h3>
         <p>Lorem ipsum dolor sit amet, consectetur </p>
       </aside>
+      <button className="btn-news">Next</button>
     </section>
   );
 }
