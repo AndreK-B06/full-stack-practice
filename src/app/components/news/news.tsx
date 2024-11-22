4e813b8b1ffb1f4159f2d6436f7844e28184c422
André <vestrheim-kvist@hotmail.com>
2024-11-22 11:28:32 +0100

    adding scroleing

--- src/app/components/news/news.tsx
+++ src/app/components/news/news.tsx
@@ -4,14 +4,29 @@ export default function news() {
   return (
     <section className="flex-row news">
       <button className="btn-news">Back</button>
-      <aside className="flex-column">
-        <h3>Patch</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur </p>
-      </aside>
-      <aside className="flex-column">
-        <h3>Patch</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur </p>
-      </aside>
+
+      <section className="flex-row news-com">
+        <aside className="flex-column">
+          <h3>Patch</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur </p>
+        </aside>
+
+        <aside className="flex-column">
+          <h3>Patch</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur </p>
+        </aside>
+
+        <aside className="flex-column">
+          <h3>Patch</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur </p>
+        </aside>
+
+        <aside className="flex-column">
+          <h3>Patch</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur </p>
+        </aside>
+      </section>
+
       <button className="btn-news">Next</button>
     </section>
   );
